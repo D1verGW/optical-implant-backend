279daa0d3d282f79da8fbf319831e78272e4fea7
Arsentii Hrybok <gribok.work@gmail.com>
2023-08-24 06:42:11 +0200

    chore: cors;

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const port = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(port);
 }
 
