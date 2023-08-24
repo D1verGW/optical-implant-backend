0b6354c2568bbefcd005ef43e4a6a176ff02fbb5
Arsentii Hrybok <gribok.work@gmail.com>
2023-08-24 06:03:43 +0200

    feat: add dockerfile;

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 3030;
+  const port = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }
